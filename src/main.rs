e3d5c033ea50e7ad438bc1fec0d225c958902e54
jaedson.serafim <jaedson33@gmail.com>
2023-05-29 11:31:10 -0300

    allowed content_type

--- src/main.rs
+++ src/main.rs
@@ -39,6 +39,7 @@ async fn main() {
 
     let cors = CorsLayer::new()
         .allow_methods([Method::GET, Method::POST])
+        .allow_headers([http::header::CONTENT_TYPE])
         .allow_origin(Any);
 
     let app = Router::new()
